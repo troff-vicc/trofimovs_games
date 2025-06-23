cf8003f0847a2031bf49285d2d8bab557e9144a7
troff_vicc <troff.vicc@gmail.com>
2025-06-23 03:23:13 +0300

    Добавлена возможность сдвигать не по 1й плитке, а всю колонку или строчку

--- game.py
+++ game.py
@@ -11,9 +11,11 @@ class Game:
         self.start_time = int(time.time())
         self.shuffle()
     
+    
     def get_time(self):
         """Получает текущее время"""
         return int(time.time()) - self.start_time
+
 
     def shuffle(self):
         """Перемешивает плитки с гарантией решаемости"""
@@ -33,23 +35,46 @@ class Game:
         if (inversions + empty_row) % 2 == 0:
             i = 2 if self.empty_pos in (0, 1) else 0
             self.tiles[i], self.tiles[i+1] = self.tiles[i+1], self.tiles[i]
-
-
+    
+    
     def move_tile(self, tile_index):
-        """Пытается переместить плитку, возвращает успешность"""
-        if ((abs(tile_index - self.empty_pos) == 1
-                and tile_index // 4 == self.empty_pos // 4)
-                or abs(tile_index - self.empty_pos) == 4):
-            
-            
-            (self.tiles[self.empty_pos],
-            self.tiles[tile_index]) =  (self.tiles[tile_index],
-                                        self.tiles[self.empty_pos])
-            self.empty_pos = tile_index
-            self.moves += 1
-            return self.moves, self.get_time()
+        """Перемещает плитку или целый ряд/столбец к пустой клетке"""
+        empty_row, empty_col = self.empty_pos // 4, self.empty_pos % 4
+        tile_row, tile_col = tile_index // 4, tile_index % 4
         
-        return False
+        # Проверяем, что плитка в одной строке или столбце с пустой клеткой
+        if not (tile_row == empty_row or tile_col == empty_col):
+            return False  # Невозможно переместить
+        
+        # Горизонтальное движение (в строке)
+        if tile_row == empty_row:
+            row = self.tiles.reshape(4, 4)[empty_row, :]
+            
+            if tile_col > empty_col:
+                row[empty_col:tile_col] = row[empty_col + 1:tile_col + 1]
+            else:
+                row[tile_col + 1:empty_col + 1] = row[tile_col:empty_col]
+            
+            row[tile_col] = 0  # Обновляем пустую клетку
+            self.tiles.reshape(4, 4)[empty_row, :] = row  # Возвращаем строку обратно
+        
+        # Вертикальное движение (в столбце)
+        elif tile_col == empty_col:
+            col = self.tiles.reshape(4, 4)[:, empty_col]
+            
+            if tile_row > empty_row:
+                col[empty_row:tile_row] = col[empty_row + 1:tile_row + 1]
+            else:
+                col[tile_row + 1:empty_row + 1] = col[tile_row:empty_row]
+                
+            col[tile_row] = 0  # Обновляем пустую клетку
+            self.tiles.reshape(4, 4)[:, empty_col] = col  # Возвращаем столбец обратно
+        
+        self.moves += abs(tile_index - self.empty_pos) if tile_row == empty_row \
+            else abs(tile_index - self.empty_pos) // 4
+        self.empty_pos = tile_index
+        
+        return True
         
 
     def is_solved(self):
